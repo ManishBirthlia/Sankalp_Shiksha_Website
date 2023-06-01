911cf9a4f0c29f0779fbb04d64ee4ee6e4e9a23f
Sidhant Sadyal <116481420+Sid0225s@users.noreply.github.com>
2023-06-01 12:51:58 +0530

    chane

--- src/React Components/Gallery/Gallery.jsx
+++ src/React Components/Gallery/Gallery.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import s from "./Gallery.module.css";
+import img4 from "./4.JPG";
+import img11 from "./11.JPG";
+import img2 from "./2.JPG";
+import img12 from "./12.JPG";
 
 function Gallery() {
   return (
@@ -10,23 +14,17 @@ function Gallery() {
         <img alt="" src="./Main_pics/1.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/16.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/15.jpg" class="img-responsive" />
-        <video class={s.vid} controls>
-          <source
-            src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-            type="video/mp4"
-          ></source>
-        </video>
         <img alt="" src="./Main_pics/14.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/13.jpg" class="img-responsive" />
-        <img alt="" src="./Main_pics/12.jpg" class="img-responsive" />
-        <img alt="" src="./Main_pics/11.jpg" class="card img-responsive" />
+        <img alt="" src={img12} class="img-responsive" />
+        <img alt="" src={img11} class="card img-responsive" />
         <img alt="" src="./Main_pics/10.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/9.jpg" class="img-responsive" />
-        <img alt="" src="./Main_pics/4.jpg" class="img-responsive" />
+        <img alt="" src={img4} class="img-responsive" />
         <img alt="" src="./Main_pics/6.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/7.jpg" class="img-responsive" />
         <img alt="" src="./Main_pics/3.jpg" class="img-responsive" />
-        <img alt="" src="./Main_pics/2.jpg" class="img-responsive" />
+        <img alt="" src={img2} class="img-responsive" />
         <img alt="" src="./Main_pics/5.jpg" class="img-responsive" />
       </div>
 
