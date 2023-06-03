bfb7f6ab59374f9536bbe6a40edeffdf4b4dd4cb
Sidhant Sadyal <116481420+Sid0225s@users.noreply.github.com>
2023-06-03 21:04:10 +0530

    ch

--- src/React Components/Founder/Founder.jsx
+++ src/React Components/Founder/Founder.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import "./Founder.css";
 
-export default () => {
+function Founder() {
   return (
-      <div className="Founder">
+    <div className="Founder">
       <h1>Founder's Story</h1>
-        <div className="Fdisplay">
-          <div className="Fimg">
-            <img alt="" src="./Main_pics/1.jpg" class="img-responsive" />
-          </div>
-          <div className="FText">
-            <h2>Name</h2>
-            <h3>Title</h3>
-            <p>Mr. Vikas Kumar, graduate of NIT Hamirpur pursuing electrical and communication engineering was inspired by the covid emergency to selflessly provide services to the people in need. He, along with some of his peers from his village distributed food-items, masks, soap and medicine while also creating awareness among the villagers. This initiative has now grown into Sankalp Shiksha which is working towards upliftment and betterment of society as a whole. Mr Kumar's efforts have been an inspiration for many others who are striving hard during this difficult time to help those in need without expecting anything in return.</p>
-          </div>
+      <div className="Fdisplay">
+        <div className="Fimg">
+          <img alt="" src="./Main_pics/1.jpg" class="img-responsive" />
+        </div>
+        <div className="FText">
+          <h2>Name</h2>
+          <h3>Title</h3>
+          <p>
+            Mr. Vikas Kumar, graduate of NIT Hamirpur pursuing electrical and
+            communication engineering was inspired by the covid emergency to
+            selflessly provide services to the people in need. He, along with
+            some of his peers from his village distributed food-items, masks,
+            soap and medicine while also creating awareness among the villagers.
+            This initiative has now grown into Sankalp Shiksha which is working
+            towards upliftment and betterment of society as a whole. Mr Kumar's
+            efforts have been an inspiration for many others who are striving
+            hard during this difficult time to help those in need without
+            expecting anything in return.
+          </p>
         </div>
       </div>
+    </div>
 
     // <>
     // <div className="Founder">
@@ -49,4 +60,5 @@ export default () => {
     // <hr/>
     // </>
   );
-};
+}
+export default Founder;
