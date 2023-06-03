c69768a1622944c30686291025a187e2c286731c
--replace-all <manishbirthliya@gmail.com>
2023-06-03 21:00:53 +0530

    u

--- src/React Components/Founder/Founder.jsx
+++ src/React Components/Founder/Founder.jsx
@@ -10,8 +10,8 @@ export default () => {
             <img alt="" src="./Main_pics/1.jpg" class="img-responsive" />
           </div>
           <div className="FText">
-            <h2>Name</h2>
-            <h3>Title</h3>
+            <h2>Vikas Kumar</h2>
+            <h3>Founder of Sankalp</h3>
             <p>Mr. Vikas Kumar, graduate of NIT Hamirpur pursuing electrical and communication engineering was inspired by the covid emergency to selflessly provide services to the people in need. He, along with some of his peers from his village distributed food-items, masks, soap and medicine while also creating awareness among the villagers. This initiative has now grown into Sankalp Shiksha which is working towards upliftment and betterment of society as a whole. Mr Kumar's efforts have been an inspiration for many others who are striving hard during this difficult time to help those in need without expecting anything in return.</p>
           </div>
         </div>
